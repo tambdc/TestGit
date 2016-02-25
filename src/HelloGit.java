0c821b2c219ac5ee43a114bd967b1bdf7991ab7c
tambdc <tambdc@gmail.com>
2016-02-25 15:31:55 +0800

    123

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("Hello again");
+		System.out.println("Hello again 1");
 	}
 
 }
